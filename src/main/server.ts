676c2ca1f25866171a60ae909f98c6d619ded488
jean.almeida <jean.almeida@pagtel.com.br>
2021-09-05 13:41:38 -0300

    chore: implement connection rabbitMq

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,21 @@
+import { RabbitMqServer } from '../infra/amqp/helper';
 import { dbPhoenix } from '../infra/core/db/helpers';
-import { SERVER } from '../utils/config/constants';
+import { RABBITMQ, SERVER } from '../utils/config/constants';
 import errorLogger from '../utils/logger';
 import { server } from './application';
 
 (async () => {
   try {
+    const rabbitMqServer = new RabbitMqServer();
+    const rabbitMq = rabbitMqServer.getInstance();
+    rabbitMq.setCredentials({
+      host: RABBITMQ.HOST,
+      password: RABBITMQ.PASSWORD,
+      port: +RABBITMQ.PORT,
+      user: RABBITMQ.USER,
+    });
+    await rabbitMq.start();
+
     await dbPhoenix.raw('SELECT 1');
 
     server.listen(SERVER.PORT, async () => {
