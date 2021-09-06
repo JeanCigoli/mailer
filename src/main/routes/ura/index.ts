0f9be58c82a52cba0f5faad9843fd86c48a423a0
jean.almeida <jean.almeida@pagtel.com.br>
2021-09-06 16:21:11 -0300

    chore: init switch middleware config

--- src/main/routes/ura/index.ts
+++ src/main/routes/ura/index.ts
@@ -1,30 +1,22 @@
 import { Router } from 'express';
-import { makeResponseXml } from '../../../utils/response/response-xml';
+import { adaptMiddlewareStep } from '../../adapters/adapt-middleware-step';
 import { adaptMiddlewareXml } from '../../adapters/adapt-middleware-xml';
+import { adaptSwitchMiddleware } from '../../adapters/adapt-switch-middleware';
+import { makeVerifyStepWhatsApp } from '../../factories/middlewares/core/make-verify-step-whats';
 import { makeXmlParser } from '../../factories/middlewares/ura';
+import {
+  stepMiddlewareSwitchConfig,
+  stepCoreSwitchConfig,
+  formatUraSwitchConfig,
+} from '../config';
 
 export default (routes: Router) => {
-  routes.post('/', adaptMiddlewareXml(makeXmlParser()), (req, res) => {
-    // console.log('CONTROLLER', req.body);
-
-    const objTest = {
-      voice: {
-        total: 1000,
-        available: 1000,
-        used: 0,
-      },
-      sms: {
-        total: 0,
-        available: 0,
-        used: 0,
-      },
-      data: {
-        total: 750,
-        available: 750,
-        used: 0,
-      },
-    };
-
-    res.send(makeResponseXml(objTest));
-  });
+  routes.post(
+    '/',
+    adaptMiddlewareXml(makeXmlParser()),
+    adaptMiddlewareStep(makeVerifyStepWhatsApp()),
+    adaptSwitchMiddleware(stepMiddlewareSwitchConfig),
+    adaptSwitchMiddleware(stepCoreSwitchConfig),
+    adaptSwitchMiddleware(formatUraSwitchConfig),
+  );
 };
