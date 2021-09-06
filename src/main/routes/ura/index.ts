24ecf4381ee1fede13fd0f94a9cd3ef940626d63
jean.almeida <jean.almeida@pagtel.com.br>
2021-09-06 20:10:47 -0300

    chore: implement ura route

--- src/main/routes/ura/index.ts
+++ src/main/routes/ura/index.ts
@@ -2,16 +2,16 @@ import { Router } from 'express';
 import { adaptMiddlewareStep } from '../../adapters/adapt-middleware-step';
 import { adaptMiddlewareXml } from '../../adapters/adapt-middleware-xml';
 import { adaptSwitchMiddleware } from '../../adapters/adapt-switch-middleware';
-import { makeVerifyStepWhatsApp } from '../../factories/middlewares/dialogue';
+import { makeVerifyStepUra } from '../../factories/middlewares/dialogue';
 import { makeXmlParser } from '../../factories/middlewares/ura';
-import { stepCoreSwitchConfig, formatUraSwitchConfig } from '../config';
+import { formatUraSwitchConfig, sourceSwitchConfig } from '../config';
 
 export default (routes: Router) => {
   routes.post(
     '/',
     adaptMiddlewareXml(makeXmlParser()),
-    adaptMiddlewareStep(makeVerifyStepWhatsApp()),
-    adaptSwitchMiddleware(stepCoreSwitchConfig),
+    adaptMiddlewareStep(makeVerifyStepUra()),
+    adaptSwitchMiddleware(sourceSwitchConfig),
     adaptSwitchMiddleware(formatUraSwitchConfig),
   );
 };
