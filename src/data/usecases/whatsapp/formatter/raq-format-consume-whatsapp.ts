44a20717da83397b8cbc8b38f5e7833b685b8902
jean.almeida <jean.almeida@pagtel.com.br>
2021-09-14 18:55:20 -0300

    refactor: implement defined source

--- src/data/usecases/whatsapp/formatter/raq-format-consume-whatsapp.ts
+++ src/data/usecases/whatsapp/formatter/raq-format-consume-whatsapp.ts
@@ -3,25 +3,27 @@ import { Source } from '../../../../utils/enum/source';
 
 export class RaqFormatConsumeWhatsApp implements FormatConsumeWhatsApp {
   format(params: any): FormatConsumeWhatsApp.Result {
-    if (params.data[0].message.type === 'TEXT') {
+    const [object] = params.data;
+
+    if (object.message.type === 'TEXT') {
       return {
-        message: params.data[0].message.messageText,
-        msisdn: params.data[0].source, // '5515976079352',
+        message: object.message.messageText,
+        msisdn: object.source, // '5515976079352',
         sourceId: Source.WHATSAPP,
       };
     }
 
-    if (params.data[0].message.interactive.type === 'LIST_REPLY') {
+    if (object.message.interactive.type === 'LIST_REPLY') {
       return {
-        message: params.data[0].message.listReply.rowIdentifier,
-        msisdn: params.data[0].source, // '5515976079352',
+        message: object.message.listReply.rowIdentifier,
+        msisdn: object.source, // '5515976079352',
         sourceId: Source.WHATSAPP,
       };
     }
 
     return {
-      message: params.data[0].message.payload,
-      msisdn: params.data[0].source, // '5515976079352',
+      message: object.message.payload,
+      msisdn: object.source, // '5515976079352',
       sourceId: Source.WHATSAPP,
     };
   }
