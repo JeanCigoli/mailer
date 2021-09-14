2f43cbd65c8f27cc8ad7d164297dbbc0d63324bf
jean.almeida <jean.almeida@pagtel.com.br>
2021-09-14 11:21:46 -0300

    chore: implement steps ura navigation

--- src/main/routes/config/step-core-ura-config.ts
+++ src/main/routes/config/step-core-ura-config.ts
@@ -2,11 +2,28 @@ import { NextFunction, Request, Response } from 'express';
 import { adaptMiddleware } from '../../adapters/adapt-middleware';
 import { adapterOptions } from '../../adapters/adapt-switch-middleware';
 import {
+  makeAddNumberCardUra,
+  makeAddSecurityCodeUra,
+  makeAddValidityCardUra,
   makeAuthenticationUra,
+  makeCardsMenuUra,
+  makeConfirmAddCardUra,
+  makeConfirmInformationNumberUra,
+  makeConfirmListValuesUra,
+  makeConfirmPaymentUra,
+  makeDeleteCardErrorUra,
+  makeDeleteCardStepUra,
+  makeDeleteCardSuccessUra,
+  makeErrorRechargeInformationUra,
+  makeInformationNumberUra,
+  makeListValuesUra,
   makeMainMenuUra,
   makeMenuRechargeUra,
   makeMenuTokenUra,
+  makeMenuTypePaymentUra,
+  makeResendInformationNumberUra,
 } from '../../factories/middlewares/core';
+import { makeViewsCardsUra } from '../../factories/middlewares/core/payment/make-views-cards';
 
 export const stepCoreUraSwitchConfig: adapterOptions = [
   {
@@ -28,6 +45,91 @@ export const stepCoreUraSwitchConfig: adapterOptions = [
     target: { step: 'stepId' },
     expected: { stepId: 8 },
     handle: adaptMiddleware(makeMenuRechargeUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 9 },
+    handle: adaptMiddleware(makeInformationNumberUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 10 },
+    handle: adaptMiddleware(makeResendInformationNumberUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 11 },
+    handle: adaptMiddleware(makeConfirmInformationNumberUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 12 },
+    handle: adaptMiddleware(makeListValuesUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 13 },
+    handle: adaptMiddleware(makeConfirmListValuesUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 14 },
+    handle: adaptMiddleware(makeMenuTypePaymentUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 16 },
+    handle: adaptMiddleware(makeViewsCardsUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 17 },
+    handle: adaptMiddleware(makeAddNumberCardUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 19 },
+    handle: adaptMiddleware(makeAddValidityCardUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 21 },
+    handle: adaptMiddleware(makeConfirmPaymentUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 22 },
+    handle: adaptMiddleware(makeAddSecurityCodeUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 24 },
+    handle: adaptMiddleware(makeErrorRechargeInformationUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 25 },
+    handle: adaptMiddleware(makeCardsMenuUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 26 },
+    handle: adaptMiddleware(makeConfirmAddCardUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 28 },
+    handle: adaptMiddleware(makeDeleteCardStepUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 29 },
+    handle: adaptMiddleware(makeDeleteCardSuccessUra()),
+  },
+  {
+    target: { step: 'stepId' },
+    expected: { stepId: 30 },
+    handle: adaptMiddleware(makeDeleteCardErrorUra()),
   },
   {
     handle: (req: Request, res: Response, next: NextFunction) => {
